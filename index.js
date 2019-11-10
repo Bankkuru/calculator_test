9af3173fc52bd4f63cd2afb06f371140e6ee2ba9
Bankkuru <wittawatcheawphoo@gmail.com>
2019-11-10 13:04:13 +0700

    again

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ Sentry.init({ dsn: 'https://e2d09f80c0b2445d84ab1fdbb54d512a@sentry.io/1814488' 
 Sentry.configureScope(function(scope) {
     scope.setTag("57355120093", "wittawatcheawphoo@gmail.com");
     scope.setUser({
-      email: "ittawatcheawphoo@gmail.com",
+      email: "wittawatcheawphoo@gmail.com",
       username: 'Wittawat Cheawphoo’'
     });
    });
