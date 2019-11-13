29f457abe7b54791ece8241daaded2568e93dc94
bankkuru <wittawatcheawphoo@gmail.com>
2019-11-14 00:58:17 +0700

    commit 3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Sentry = require('@sentry/node');
 Sentry.init({ dsn: 'https://e2d09f80c0b2445d84ab1fdbb54d512a@sentry.io/1814488' });
 
 Sentry.configureScope(function(scope) {
-    scope.setTag("57355120093", "wittawatcheawphoo@gmail.com");
+    scope.setTag("57355120093", "Wittawat Cheawphoo");
     scope.setUser({
       email: "wittawatcheawphoo@gmail.com",
       username: 'Wittawat Cheawphoo'
